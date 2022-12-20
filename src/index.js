4e72396b6a880e18442d9a6043e6935744193cf2
ferdinandlim1 <ferdinand6@student.umn.ac.id>
2022-12-20 21:17:20 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from '/components/login';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Login />
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
